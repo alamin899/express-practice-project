1dc6a31edd8054ae3f8f33763db088c8f0d13c55
alaminAppnap <alamin@appnap.io>
2022-10-18 14:30:09 +0600

    get mongo all data and specific data

--- routes/mongoose-crud.js
+++ routes/mongoose-crud.js
@@ -7,15 +7,50 @@ const todoSchema = require("../schemas/todoSchema");
 /** Create Todo mode */
 const Todo = new mongoose.model("Todo",todoSchema); /** 1st paramter model name, alawys singular it will create table plular like laravel migration */
 
+
 /** Get all todos  */
 mongoCrudRoter.get('/',async(req,res)=>{
-  
+    await Todo.find({},(err,data)=>{
+        if(err){
+            res.status(500).json({
+                error:"there was a server side error"
+            });
+        }
+        else{
+            res.status(200).json({
+                data:data,
+                message:"Success"
+            })
+        }
+    })
 });
+
+/** Get selected todos todos  */
+mongoCrudRoter.get('/specific',async(req,res)=>{
+    await Todo.find({status:"active"}).select({  /** eibabe specific data pete pari chining er maddome */
+        _id:0,
+        __v:0
+    }).exec((err,data)=>{
+        if(err){
+            res.status(500).json({
+                error:"there was a server side error"
+            });
+        }
+        else{
+            res.status(200).json({
+                data:data,
+                message:"Success"
+            })
+        }
+    })
+});
+
 
 /** Find single todos */
 mongoCrudRoter.get('/:todo_id',async(req,res)=>{
 
 });
+
 
 /** Store single todos */
 mongoCrudRoter.post('/',async(req,res)=>{
@@ -54,9 +89,30 @@ mongoCrudRoter.post('/multiple',async(req,res)=>{
 });
 
 
+
 /** Update multiple todos */
 mongoCrudRoter.put('/:todo_id',async(req,res)=>{
-
+    /** we can use Todo.findByIdAndUpdate() eibabe amra await ke jodi variable a nei tahole update er por data dibe*/
+    await Todo.updateOne({
+        _id:req.params.todo_id
+    },{
+        $set:{
+            title:req.body.title,
+            description:req.body.description,
+            status:req.body.status,
+        }
+    },(err)=>{
+        if(err){
+            res.status(500).json({
+                error:"there was a server side error"
+            });
+        }
+        else{
+            res.status(200).json({
+                message:"Todo updated successfully"
+            })
+        }
+    })
 });
 
 
