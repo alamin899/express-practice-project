cdd4700e1a85e73e8d70cab1546e5cc53ec1c317
alaminhossen899 <alaminhossen899@gmail.com>
2022-10-18 07:51:20 +0600

    create todo schema like migration

---
+++ schemas/todoSchema.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const todoSchema = mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    status:{
+        type:String,
+        enum:["active","inactive"]
+    },
+    created_at:{
+        type:Date,
+        default:Date.now
+    }
+
+});
